079757e0f512ec382fef83dc7c8ee1b6fcd280d0
Thanakorn.A <kalostn2099@hotmail.com>
2019-03-07 17:04:12 +0700

    add index.js and node_modules

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 7777;
+
+app.get('/', function (req, res){
+    res.send('Hello')
+});
+
+app.listen(port, function() {
+    console.log('Start NodeJS on Port: ' + port);
+});
